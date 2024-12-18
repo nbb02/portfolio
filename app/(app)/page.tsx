f58e76ddd834b05665fa13ce5a90b5c063f8ecb6
Nathaniel Berres <berresn050@gmail.com>
2024-12-18 15:51:12 +0800

    viewing

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,6 +3,8 @@ import { signOut } from "../login/actions"
 import { db } from "@/src"
 import { profiles } from "@/src/db/schema"
 import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+import Link from "next/link"
 
 export default async function Home() {
   const all_profiles = await db.select().from(profiles)
@@ -13,8 +15,6 @@ export default async function Home() {
   } = await supabase.auth.getUser()
 
   const hasProfile = all_profiles?.find((item) => item.user_id === user?.id)
-
-  console.log(all_profiles)
 
   return (
     <div className="flex-1 flex flex-col min-h-screen  w-full border-2 border-violet-600 border-solid">
@@ -52,20 +52,45 @@ export default async function Home() {
           )}
         </header>
         <main className=" flex-1 flex gap-2 py-2 px-5 flex-wrap content-center bg-fuchsia-200">
-          {<UserBox index={0} />}
+          {all_profiles.map((userData) => (
+            <UserBox key={userData.id} userData={userData} />
+          ))}
         </main>
       </main>
     </div>
   )
 }
 
-function UserBox({ index }: { index: number }) {
+function UserBox({ userData }: { userData: UserData }) {
   return (
-    <div
-      key={index}
-      className="min-w-[20em] h-[20em] flex-1 border-blue-400 border-solid border-2 rounded-lg"
-    >
-      {index}
+    <div className="min-w-[20em] h-[20em] flex-1 border-blue-400 border-solid border-2 rounded-lg">
+      <img src={userData?.avatar_image ?? ""} alt="" />
+      <img
+        className="h-[10em] w-[15em] object-cover"
+        src={userData?.cover_photo ?? ""}
+        alt=""
+      />
+      <h2>{userData.first_name}</h2>
+      <h3>{userData.last_name}</h3>
+      <p>{userData.role}</p>
+      <Link href={"/" + userData.user_id}>View</Link>
     </div>
   )
+}
+
+type UserData = {
+  user_id: string
+  id: number
+  first_name: string
+  middle_name?: string | null
+  last_name: string
+  email: string
+  avatar_image: string | null
+  cover_photo: string | null
+  role: string | null
+  about: string | null
+  country: string | null
+  province: string | null
+  city: string | null
+  created_at: Date
 }
